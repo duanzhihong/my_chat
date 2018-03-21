f8af7f2a3db9f0cde267831fe6bc601f5eab93dc
duanzhihong <d_zhi_hong@163.com>
2018-03-21 14:27:33 +0800

    图片和图文的添加

--- wechat.class.php
+++ wechat.class.php
@@ -8,13 +8,6 @@
 require_once './wechat.cfg.php';
 class wechat
 {
-    //设置6个数值，在回复消息的时候使用
-    public $fromUsername;//设置发送方（接收方）
-    public $toUsername;  //设置接收方（发送方）
-    public $msgType;     //设置发送的类型
-    public $time;        //设置消息的时间
-    public $keyword;     //接受到的内容
-    public $msgId;       //消息的id
     public function valid()
     {
         $echoStr = $_GET["echostr"];
@@ -69,22 +62,32 @@ class wechat
                the best way is to check the validity of xml by yourself */
             libxml_disable_entity_loader(true);
             $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-            $FromUserName = $postObj->FromUserName;
-            $ToUserName = $postObj->ToUserName;
-            $keyword = trim($postObj->Content);
-            $CreateTime = time();
-           // $msgType=$postObj->MsgType; 暂时不使用。
-            $MsgId=$postObj->MsgId;
+            $FromUserName = $postObj->FromUserName; //用户的标识
+            $ToUserName = $postObj->ToUserName;  //微信服务器的标识
+            $keyword = trim($postObj->Content);//用户发送过来的内容
+            $MsgId=$postObj->MsgId; //消息的id
             if(!empty( $keyword ))
             {
                 switch($keyword)
                 {
                    case '文本';
-                       $this->sendText($ToUserName,$FromUserName,$CreateTime,$msgType='text',$contentStr='',$MsgId);
+                       $this->sendText($postObj);
                        break;
                    case '图片';
-                       $this->sendImg($ToUserName,$FromUserName,$CreateTime,$MsgType='image',$PicUrl='',$MediaId='',$MsgId);
+                       $this->sendImg($postObj);
                        break;
+                   case '语音';
+                       $this->sendVoice($postObj);
+                       break;
+                   case '视频';
+                       $this->sendVideo($postObj);
+                       break;
+                   case '音乐';
+                       $this->sendMusic($postObj);
+                       break;
+                    case '图文';
+                        $this->sendImgText($postObj);
+                        break;
                 }
             }else{
                 echo "Input something...";
@@ -98,15 +101,10 @@ class wechat
 
 
     /**
-     * 回复文本使用的模板
-     * @param $fromUsername //接受方
-     * @param $toUsername //发送方
-     * @param $time //消息创建时间
-     * @param string $msgType //消息类型
-     * @param string $contentStr //消息内容
-     * @param $msgId //消息id
+     * 回复文本消息
+     * @param $postObj
      */
-    public function sendText($toUsername,$fromUsername,$time,$msgType='text',$contentStr='',$msgId)
+    public function sendText($postObj)
     {
         $textTpl = "<xml>
                         <ToUserName><![CDATA[%s]]></ToUserName>
@@ -117,32 +115,74 @@ class wechat
                         <MsgId>%s</MsgId>
 					</xml>";
         $msgType = "text";
-        $contentStr = "Welcome to wechat world!";
-        $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr,$msgId);
+        $contentStr = "Welcome to duanzhihong world!";
+        $resultStr = sprintf($textTpl, $postObj->FromUserName, $postObj->ToUserName,time(), $msgType, $contentStr,$postObj->MsgId);
         echo $resultStr;
     }
 
 
-
-    public function sendImg($ToUserName,$FromUserName,$CreateTime,$MsgType='image',$PicUrl='',$MediaId='',$MsgId)
+    /**
+     * 回复图片消息
+     * @param $postObj
+     */
+    public function sendImg($postObj)
     {
-        $textTpl="<xml>
-                        <ToUserName>< ![CDATA[%s] ]></ToUserName>
-                        <FromUserName>< ![CDATA[%s] ]></FromUserName>
+        $imgTpl="<xml>
+                        <ToUserName><![CDATA[%s]]></ToUserName>
+                        <FromUserName><![CDATA[%s]]></FromUserName>
                         <CreateTime>%s</CreateTime>
-                        <MsgType>< ![CDATA[%s] ]></MsgType>
-                        <PicUrl>< ![CDATA[%s] ]></PicUrl>
-                        <MediaId>< ![CDATA[%s] ]></MediaId>
-                        <MsgId>%s</MsgId>
-                  </xml>";
-        $PicUrl='rU4ZKheoUu2MBu7y9ZwuX8pbPFBcKuLhoyi6rjrOCZxDVF4uJJogulQpseBjP_eX';
+                        <MsgType><![CDATA[%s]]></MsgType>
+                        <Image>
+                            <MediaId><![CDATA[%s]]></MediaId>
+                        </Image>
+                 </xml>";
+        $MsgType='image'; //返回的文件的类型、
         $MediaId='rU4ZKheoUu2MBu7y9ZwuX8pbPFBcKuLhoyi6rjrOCZxDVF4uJJogulQpseBjP_eX'; //多媒体上传接口id
-        $resultStr=sprintf($textTpl,$FromUserName,$ToUserName,$CreateTime,$MsgType,$PicUrl,$MediaId,$MsgId);
+        $resultStr=sprintf($imgTpl,$postObj->FromUserName,$postObj->ToUserName,time(),$MsgType,$MediaId);
         echo $resultStr;
     }
 
+    /**
+     * 回复语音消息
+     */
 
+    /**
+     * 回复视频消息
+     */
 
+    /**
+     * 回复音乐消息
+     */
+
+    /**
+     * 回复单个图文消息
+     * @param $postObj
+     */
+    public function sendImgText($postObj)
+    {
+          $imgTextTel="<xml>
+                        <ToUserName><![CDATA[%s]]></ToUserName>
+                        <FromUserName><![CDATA[%s]]></FromUserName>
+                        <CreateTime>%s</CreateTime>
+                        <MsgType><![CDATA[%s]]></MsgType>
+                        <ArticleCount>1</ArticleCount>
+                        <Articles>
+                            <item>
+                                <Title><![CDATA[%s]]></Title>
+                                <Description><![CDATA[%s]]></Description>
+                                <PicUrl><![CDATA[%s]]></PicUrl>
+                                <Url><![CDATA[%s]]></Url>
+                            </item>
+                        </Articles>
+                       </xml>";
+          $MsgType='news';
+          $Title='我在北京';
+          $Description='今天是03月21号';
+          $PicUrl='http://47.94.170.230/wechat/image/test.jpg';
+          $Url='http://www.baidu.com';
+          $resultStr=sprintf($imgTextTel,$postObj->FromUserName,$postObj->ToUserName,time(),$MsgType,$Title,$Description,$PicUrl,$Url);
+          echo $resultStr;
+    }
 
 
 
